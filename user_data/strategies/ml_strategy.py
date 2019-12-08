b7fd1e040744c0f23a9a6a5e18883f1eebb33133
cryofrost <cryofrost@yandex.ru>
2019-12-08 16:00:34 +0300

    just look at this graph! added news analysis

--- user_data/strategies/ml_strategy.py
+++ user_data/strategies/ml_strategy.py
@@ -1,26 +1,44 @@
-
+"""
+Strategy based on machine learning algorythms
+"""
 # --- Do not remove these libs ---
-from freqtrade.strategy.interface import IStrategy
-from typing import Dict, List
-from functools import reduce
-from pandas import DataFrame
-import pandas as pd
-from sklearn.model_selection import train_test_split
+# import freqtrade.vendor.qtpylib.indicators as qtpylib
+import glob
+import logging
+import time
+import warnings
+# from typing import Dict, List
+# from functools import reduce
 # from sklearn.ensemble import GradientBoostingRegressor
 # from sklearn.externals import joblib
 # from sklearn.preprocessing import Imputer
 
-# from sklearn.neighbors import KNeighborsRegressor
-from sklearn.pipeline import make_pipeline, make_union
-from sklearn.preprocessing import Binarizer, MinMaxScaler
-from sklearn.tree import DecisionTreeRegressor
-from tpot.builtins import StackingEstimator, ZeroCount
+# from sklearn.pipeline import make_pipeline, make_union
+# from sklearn.preprocessing import Binarizer, MinMaxScaler
+# from sklearn.tree import DecisionTreeRegressor
+# from tpot.builtins import StackingEstimator, ZeroCount
 
 # --------------------------------
 
-import talib.abstract as ta
-import freqtrade.vendor.qtpylib.indicators as qtpylib
+from datetime import datetime
+from datetime import timezone
+from time import mktime
+from pandas import DataFrame
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.preprocessing import MinMaxScaler
+import nltk
 import numpy # noqa
+import feedparser
+from bs4 import BeautifulSoup
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import pandas as pd
+import talib.abstract as ta
+from freqtrade.strategy.interface import IStrategy
+
+logger = logging.getLogger('ML Strategy')
+warnings.filterwarnings('ignore')
+nltk.download('vader_lexicon', download_dir='./user_data/nltk_data/')
 
 
 class MLStrategy(IStrategy):
@@ -81,14 +99,17 @@ class MLStrategy(IStrategy):
             from freqtrade.resolvers import ExchangeResolver
             from freqtrade.data.dataprovider import DataProvider
 
-            exchange = ExchangeResolver(self.config['exchange']['name'], self.config, False).exchange
+            exchange = ExchangeResolver(
+                self.config['exchange']['name'], self.config, False).exchange
             dataprovider = DataProvider(self.config, exchange)
             self.dp = dataprovider
+
         self.train()
 
     def train(self):
-        # Trains a model
-        import time
+        """
+        Trains a model
+        """
         start = time.time()
         response = "Train failure"
 
@@ -116,10 +137,11 @@ class MLStrategy(IStrategy):
                         # 'fastd', 'fastk',
                         # 'sar', 'sma'
                         ]
-        from sklearn.preprocessing import MinMaxScaler
 
         df_filtered = df.drop(self.to_drop, axis=1)
-        self.fr_features = ['high', 'low', 'open', 'close', 'volume', 'minus_di', 'rsi', 'fastd', 'fastk', 'sar', 'sma', 'TRANGE']
+        self.fr_features = ['high', 'low', 'open', 'close',
+                            'volume', 'minus_di', 'rsi', 'fastd', 'fastk',
+                            'sar', 'sma', 'TRANGE']
         self.neg_features = ['macd']
         # df[['x','z']] = mms.fit_transform(df[['x','z']])
         self.fr_scaler = MinMaxScaler(feature_range=(0, 1))
@@ -143,7 +165,7 @@ class MLStrategy(IStrategy):
         #     print('test:', testing_features[i], testing_target[i])
         # from tpot import TPOTRegressor
         # import sys
-        # # my_tpot = TPOTRegressor(generations=10, verbosity=2, n_jobs=-2, config_dict='TPOT light')
+        # #my_tpot = TPOTRegressor(generations=10, verbosity=2, n_jobs=-2, config_dict='TPOT light')
         # my_tpot = TPOTRegressor(generations=10, verbosity=2, n_jobs=-2)
         # my_tpot.fit(training_features, training_target)
         # print(my_tpot.score(testing_features, testing_target))
@@ -173,8 +195,7 @@ class MLStrategy(IStrategy):
         #                     #   subsample=0.55
         #                          )
 
-        
-        from sklearn.neighbors import KNeighborsRegressor
+        # from sklearn.neighbors import KNeighborsRegressor
         model = KNeighborsRegressor(n_neighbors=3, p=2, weights="distance", n_jobs=4)
         model.fit(training_features, training_target)
         response = 'Model fitted using dataframe of length = '\
@@ -200,6 +221,73 @@ class MLStrategy(IStrategy):
                             ]
         """
         return []
+
+    def analyze_news(self, dataframe: DataFrame):
+        """
+        Download, parse and do sentiment analysis
+        """
+        entries = []
+        sia = SentimentIntensityAnalyzer()
+        # # stock market lexicon
+        stock_lex = pd.read_csv('./user_data/lexicon_data/stock_lex.csv')
+        stock_lex['sentiment'] = (stock_lex['Aff_Score'] + stock_lex['Neg_Score'])/2
+        stock_lex = dict(zip(stock_lex.Item, stock_lex.sentiment))
+        stock_lex = {k:v for k,v in stock_lex.items() if len(k.split(' '))==1}
+        stock_lex_scaled = {}
+        for k, v in stock_lex.items():
+            if v > 0:
+                stock_lex_scaled[k] = v / max(stock_lex.values()) * 4
+            else:
+                stock_lex_scaled[k] = v / min(stock_lex.values()) * -4
+
+        final_lex = {}
+        final_lex.update(stock_lex_scaled)
+        final_lex.update(sia.lexicon)
+        sia.lexicon = final_lex
+
+        if not self.is_trained:
+            for filename in glob.iglob("./user_data/news/" + '**/*.html', recursive=True):
+                logger.info('parsing %s', filename)
+                nf = feedparser.parse(filename)
+                entries += nf.entries
+            self.training_news = entries
+        else:
+            if self.dp.runmode.value in ('backtest'):
+                entries = self.training_news
+            else:
+                feed_url = "https://news.bitcoin.com/feed/"
+                logger.info('parsing %s', feed_url)
+                nf = feedparser.parse(feed_url)
+                entries = nf.entries
+
+
+        dataframe = dataframe.set_index("date", drop=False)
+        col = 'sentiment'
+        dataframe = dataframe.assign(**{col:numpy.full(len(dataframe), numpy.nan)})
+
+        for entry in entries:
+            html = entry.content[0]['value']
+            link_soup = BeautifulSoup(html)
+            sentences = link_soup.findAll("p")
+            passage = ""
+            for sentence in sentences:
+                passage += sentence.text
+           
+            sentiment = sia.polarity_scores(passage)['compound']
+            dt = datetime.fromtimestamp(mktime(entry['published_parsed']))
+            pd_dt = pd.Timestamp(dt.replace(tzinfo=timezone.utc), freq='T')
+            idx = dataframe.index.get_loc(pd_dt, method='nearest')
+            candle_date = dataframe.iloc[idx].date
+            dataframe.loc[(dataframe.date == candle_date), col] = sentiment
+
+            logger.info("Got alfa {} for candle at {}".format(sentiment, candle_date))
+
+        # from sklearn.impute import SimpleImputer
+        # imp = SimpleImputer(missing_values='NaN', strategy='mean')
+        # sentiments = pd.np.array(dataframe.sentiment).reshape(-1, 1)
+        dataframe = dataframe.fillna(method='ffill')
+        dataframe = dataframe.fillna(method='bfill')
+        return dataframe
 
     def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
         """
@@ -239,11 +327,15 @@ class MLStrategy(IStrategy):
         dataframe['sar'] = ta.SAR(dataframe)
 
         # SMA - Simple Moving Average
-        dataframe['sma'] = ta.SMA(dataframe, timeperiod=40)      
+        dataframe['sma'] = ta.SMA(dataframe, timeperiod=40)
         # dataframe['close_to_sma'] = dataframe['close'] / dataframe['sma']
 
         dataframe['TRANGE'] = ta.TRANGE(dataframe)
         dataframe['perc_change'] = 100 * (dataframe['close'] - dataframe['open']) / dataframe['open']
+
+        # News feed analysis
+        dataframe = self.analyze_news(dataframe)
+        # dataframe['alfa'] = alfa['sentiment']
 
         if self.is_trained:
             dataframe = dataframe.dropna()
@@ -298,7 +390,7 @@ class MLStrategy(IStrategy):
             # (
             #     (qtpylib.crossed_above(dataframe['rsi'], 50)) &
             #     (dataframe['macd'] < 0) &
-            #     (dataframe['minus_di'] > 0) 
+            #     (dataframe['minus_di'] > 0)
             # ) |
             # (
             #     (dataframe['sar'] > dataframe['close']) &
